e88c528614eac7fd282c87d5b54de42c35c97604
Daniel van Dorp <daniel.van.dorp@mendix.com>
2017-02-15 13:35:33 +0100

    pimp remote_windows.sh with anti-aliasing and non-floating dimensions

--- bin/remote_windows.sh
+++ bin/remote_windows.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xfreerdp +clipboard /w:1920 /h:1050 /v:10.140.11.75:3389 /d:MENDIXDOMAIN /u:ddo /p:$(zenity \
+xfreerdp +clipboard +fonts /w:1920 /h:1040 /v:10.140.11.75:3389 /d:MENDIXDOMAIN /u:ddo /p:$(zenity \
     --entry \
     --title="Password" \
     --text="Enter your _password:" \
