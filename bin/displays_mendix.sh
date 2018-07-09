7c75383c6ea017b2ec1f0495987cf99fbfebdc67
Daniel van Dorp <daniel.van.dorp@mendix.com>
2018-07-09 10:08:11 +0200

    Fix display configs

--- bin/displays_mendix.sh
+++ bin/displays_mendix.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 xrandr --output eDP1 --mode 2560x1440
-xrandr --output DP2-1 --mode 1920x1200 --right-of eDP1
-xrandr --output DP2-2 --mode 1920x1200 --right-of DP2-1
+xrandr --output DP2-1-1 --mode 1920x1080 --left-of eDP1
+xrandr --output DP2-2-1 --mode 1920x1080 --left-of DP2-1-1
