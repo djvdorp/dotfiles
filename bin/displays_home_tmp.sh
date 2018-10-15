95125ba4fdbc3a0bcdaa42e0154558a9cd3ef668
Daniel van Dorp <daniel.van.dorp@mendix.com>
2018-10-15 11:17:27 +0200

    DO: fix monitor configs, packages backup

---
+++ bin/displays_home_tmp.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output eDP1 --mode 2560x1440
+xrandr --output HDMI2 --mode 1440x900 --above eDP1
--- bin/displays_mendix.sh
+++ bin/displays_mendix.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 xrandr --output eDP1 --mode 2560x1440
-xrandr --output DP2-1-1 --mode 1920x1080 --left-of eDP1
-xrandr --output DP2-2-1 --mode 1920x1080 --left-of DP2-1-1
+xrandr --output DP2-1 --mode 2560x1440 --left-of eDP1
+xrandr --output DP2-2 --mode 2560x1440 --left-of DP2-1
